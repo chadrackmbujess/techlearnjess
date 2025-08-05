982de1bf64bc12687d19c784ff6177e0b2e6d621
chadrackmbujess <chadrackmbujess@gmail.com>
2025-08-05 05:23:14 +0100

    techlearnjess_

--- techlearnjess/asgi.py
+++ techlearnjess/asgi.py
@@ -8,12 +8,15 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'techlearnjess.settings')
+django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
